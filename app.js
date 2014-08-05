6a4b15cae120f1a94b5af03eb5e620d955960a69
Matt Livingston <matt.livingston@qubitdigital.com>
2014-08-05 22:41:11 +0100

    Find sentences for list of kanji now working! (Alpha)

--- app.js
+++ app.js
@@ -3,11 +3,91 @@ var _            = require("lodash");
 var cheerio      = require("cheerio");
 var fs           = require("fs");
 var utils        = require("./utils");
+var wanakana     = require("./wanakana.min");
 var wkApiWrapper = require("./wk");
 
 var wk = wkApiWrapper({
   apiKey: "57a8b351b5b361b8d83d65f02ef07def"
 });
+
+utils.readFromFile("sentences.json").done(function(sentences) {
+  // wk.getKanji().done(function(kanjis) {
+  //   findSentences(sentences, createKanjiMaps(kanjis));
+  // });
+  utils.readFromFile("kanjis.json").done(function(kanjis) {
+    findSentences(sentences, createKanjiMaps(kanjis));
+  });
+});
+
+// Sentences is a map from character to array of sentences containing that character
+function findSentences(sentences, kanjiMap) {
+
+  var sentenceTotal = 0;
+  // For each kanji that this user has learned so far
+  _.each(kanjiMap.srsToKanji, function(kanjis, srs) {
+    // For each kanji in this srs level
+    _.each(kanjis, function(kanji) {
+
+      var matchedSentences = [];
+
+      // For each sentence in the list of sentences for this character, find all applicable sentences
+      _.each(sentences[kanji.character], function(sentence) {
+
+        var count = {
+          apprentice: 0,
+          guru: 0,
+          master: 0,
+          enlighten: 0,
+          burned: 0,
+          unknown: 0
+        };
+        var tempSentence = sentence.jp;
+        tempSentence = stripNonKanji(tempSentence).split("");
+        // For each character in this sentence...
+        _.each(tempSentence, function(character) {
+          // Ignore kana
+          if (!wanakana.isKana(character)) {
+            var srsLevel = kanjiMap.kanjiToSrs[character];
+            if (_.isString(srsLevel)) {
+              count[srsLevel]++;
+            }
+            else {
+              count["unknown"]++;
+            }
+          }
+        });
+        if (count.apprentice <= 1 && count.guru <= 2 && count.enlighten <= 3 && count.unknown === 0) {
+          sentence.counts = count;
+          matchedSentences.push(sentence);
+        }
+      });
+
+      sentenceTotal += matchedSentences.length;
+      kanji.sentences = matchedSentences;
+    });
+  });
+
+  console.log(kanjiMap.srsToKanji);
+  console.log(sentenceTotal);
+
+  // var gurued = {},
+  //     filtered = [];
+
+  // sentences.forEach(function(sentence) {
+
+  //   var kanjis = [];
+  //   for (var i = 0, len = sentence.length; i < len; i++) {
+  //     // add unknown kanji
+  //     if (typeof gurued[sentence[i]] === "undefined") {
+  //       kanjis.push(sentence[i]);
+  //     }
+  //   }
+  //   // only include sentences with 2 or less known kanji
+  //   if (kanjis.length <= 2) {
+  //     filtered.concat(kanjis);
+  //   }
+  // });
+}
 
 function scraper() {
 
@@ -20,10 +100,14 @@ function scraper() {
       var maps = createKanjiMaps(kanjis);
       var diff = _.difference(_.keys(maps.kanjiToSrs), alreadyStored);
 
-      getSentences(diff).done(function(sentences) {
+      scrapeSentences(diff).done(function(sentences) {
 
         // Don't save sentences that are null
         sentences = _.filter(sentences, function(sentence) { return !_.isNull(sentence); });
+        // var kanjiToSentence = {};
+        // _.each(sentences, function(sentence) {
+        //   kanjiToSentence[sentence.character] = sentence.sentences;
+        // });
         fs.writeFile("sentences.json", JSON.stringify(sentences, null, 4), function(err) {
           if (err) {
             console.error(err)
@@ -36,7 +120,7 @@ function scraper() {
 }
 
 // Scrape sentences from tangorin for each kanji in the list and return an array of promises
-function getSentences(kanjiList) {
+function scrapeSentences(kanjiList) {
 
   var promises = utils.mapSeries(kanjiList, function(kanji, index) {
 
@@ -79,6 +163,12 @@ function pluckCharacters(collection) {
   return arr;
 }
 
+function stripNonKanji(sentence) {
+  sentence = sentence.replace(/\[[^\].]*\]/g, "");
+  sentence = sentence.replace(/[。、？ー\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  return sentence;
+}
+
 function createKanjiMaps(kanjis) {
   var kanjiMaps = {
     kanjiToSrs: {},
@@ -94,33 +184,9 @@ function createKanjiMaps(kanjis) {
   kanjis.forEach(function(kanji) {
     if (kanji.user_specific !== null) {
       kanjiMaps.kanjiToSrs[kanji.character] = kanji.user_specific.srs;
-      kanjiMaps.srsToKanji[kanji.user_specific.srs].push(kanji.character);
+      kanjiMaps.srsToKanji[kanji.user_specific.srs].push({"character": kanji.character});
     }
   });
 
   return kanjiMaps;
 }
-
-// Obtain from various sources...
-var sentences = [
-  "おご名前なまえと部屋へや番号ばんごうをお願いしますおねがいします。"
-];
-
-//var gurued = wanikani.api.get("gurued");
-var gurued = {},
-    filtered = [];
-
-sentences.forEach(function(sentence) {
-
-  var kanjis = [];
-  for (var i = 0, len = sentence.length; i < len; i++) {
-    // add unknown kanji
-    if (typeof gurued[sentence[i]] === "undefined") {
-      kanjis.push(sentence[i]);
-    }
-  }
-  // only include sentences with 2 or less known kanji
-  if (kanjis.length <= 2) {
-    filtered.concat(kanjis);
-  }
-});
